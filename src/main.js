3b522b29326692b9edfff487ce59c6f5de616f19
Milmoody <moody.lillian@gmail.com>
2019-07-11 14:55:46 -0700

    MVP reached

--- src/main.js
+++ src/main.js
@@ -5,16 +5,85 @@ class Data {
         this.time = Date.now(),
         this.visits = 1,
         this.url = window.location.href
+      }
+      
+      
     }
-
+    const pickles = new Data();
+    let update;
     
-}
-const pickles = new Data();
-var test = pickles.domain.href;
-let domainText = JSON.stringify(pickles.domain)
-let update;
-/*
-// test reset
+    // resets the data in storage and store a date value in ms
+    function resetData(){
+      chrome.storage.local.set({"data":[]}, ()=>{console.log("reset hit")});
+      chrome.storage.local.set({"lastReset": `${new Date()}`});
+    }
+    // creates a reset button in popup.html
+    let resetButton = document.createElement("BUTTON");
+    let buttonText = document.createTextNode("Reset");
+    resetButton.addEventListener("click", resetData); 
+    resetButton.appendChild(buttonText);
+    //
+    
+    // ***  grabs data from storage
+    window.onload = chrome.storage.local.get(['data', 'lastReset'], function(domain){
+      console.log(domain);
+      console.log(Array.isArray(domain.data));
+      
+      let visitCount = 1;
+      for(let item of domain.data){
+        if(item.domain === document.domain){
+          visitCount++;
+        }
+      }
+      pickles.visits = visitCount;
+    
+      //if current domain isn't popup, push domain info to data
+      if(document.domain !== "eimgppkmnopgdadgbffgccnflanmjonh"){
+        domain.data.push(pickles)
+        update = domain.data;
+        window.onload = chrome.storage.local.set({"data":update});
+      }
+      
+      //adding domain element, excluding popup domain
+      if(document.domain === "eimgppkmnopgdadgbffgccnflanmjonh"){
+        //create popup in DOM
+        const popup = document.getElementById('popup');
+        //create domain name element in DOM
+        const domainEl = document.createElement("p");
+        domainEl.textContent = domain.data[domain.data.length - 1].domain;
+        popup.appendChild(domainEl);
+        //
+
+        //create visits count element in DOM
+        const visitsEl = document.createElement("p");
+        const visits = document.getElementById('visits');
+        visitsEl.textContent = domain.data[domain.data.length - 1].visits;
+        visits.appendChild(visitsEl);
+        //
+
+        //create last reset element in DOM
+        const lastResetEl = document.createElement("p");
+        const lastResetText = document.getElementById('last-reset');
+        lastResetEl.textContent = domain.lastReset;
+        lastResetText.appendChild(lastResetEl);
+        //
+
+        // creates the reset button at the bottom of the popup.html
+        const button = document.getElementById('reset');
+        console.log("inside the popup");
+        console.log(button);
+        document.body.appendChild(resetButton);
+        //
+      }
+      
+      
+    });
+    // *** all of get data *** //
+
+    // var test = pickles.domain.href;
+    // let domainText = JSON.stringify(pickles.domain)
+    /*
+    // test reset
 console.log(document.getElementById("reset"));
 document.getElementById("reset").addEventListener("click", function() {
   chrome.storage.local.set(
@@ -39,71 +108,10 @@ window.onload = () => {
   )
 }
 */
-<<<<<<< HEAD
-// chrome.storage.local.set({"data":[]}, ()=>{console.log("reset hit")});
-  // chrome.storage.local.set({"lastReset": `${new Date()}`});
-
-
-window.onload = () => {
-  function resetData(){
-    console.log("hi")
-  }
-  let resetButton = document.createElement("BUTTON");
-  let buttonText = document.createTextNode("Reset");
-  resetButton.addEventListener("click", resetData); 
-  resetButton.appendChild(buttonText);
-  document.body.appendChild(resetButton);
-}  
-=======
 
 
 
 // chrome.storage.local.set({"data":[]})
-function resetData(){
-  chrome.storage.local.set({"data":[]}, ()=>{console.log("reset hit")});
-  chrome.storage.local.set({"lastReset": `${new Date()}`});
-}
-let resetButton = document.createElement("BUTTON");
-let buttonText = document.createTextNode("Reset");
-resetButton.addEventListener("click", resetData); 
-resetButton.appendChild(buttonText);
->>>>>>> 84bd7199ef2ea1755a47b93b05bc9609071d0c48
-
-window.onload = chrome.storage.local.get(['data'], function(domain){
-  console.log(domain);
-  console.log(Array.isArray(domain.data));
-  
-  let visitCount = 1;
-  for(let item of domain.data){
-    if(item.domain === document.domain){
-      visitCount++;
-    }
-  }
-  pickles.visits = visitCount;
-  
-  //adding domain element, excluding popup domain
-  if(document.domain === "fniinplphnaobhbaobohdlgbhkkpgnaj"){
-    const button = document.getElementById('reset');
-    console.log("inside the popup");
-    console.log(button);
-    const domainEl = document.createElement("p");
-    domainEl.textContent = domain.data[domain.data.length - 1].domain;
-    const popup = document.getElementById('popup');
-    popup.appendChild(domainEl);
-    const visitsEl = document.createElement("p");
-    const visits = document.getElementById('visits');
-    visitsEl.textContent = domain.data[domain.data.length - 1].visits;
-    visits.appendChild(visitsEl);
-    document.body.appendChild(resetButton);
-  }
-  //if current domain isn't popup, push domain info to data
-  if(document.domain !== "fniinplphnaobhbaobohdlgbhkkpgnaj"){
-    domain.data.push(pickles)
-    update = domain.data;
-    window.onload = chrome.storage.local.set({"data":update});
-  }
-  
-});
 
 
 // window.onload = () => {
