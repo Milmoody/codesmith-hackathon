11e3c3510c2ebc183a01c7864776db1e59f43814
Alexander <alxdip123@gmail.com>
2019-07-10 20:46:30 -0700

    added bases

---
+++ src/main.js
@@ -0,0 +1,32 @@
+// to store current url
+window.location.href
+
+class Data {
+    constructor () {
+        this.domain = window.location.href,
+        this.time = 0,
+        this.visits = null,
+        this.extendedDomains = null
+    }
+
+    
+}
+
+chrome.storage.local.set({"domain": "Example Domain"});
+
+let testEntry = new Data();
+console.log(testEntry);
+
+//get data from JSON file with XML HTTP Request
+let getData = new XMLHttpRequest();
+getData.open('GET', '/history/data.json', true);
+
+getData.onload = function () {
+    // Convert JSON data to an object
+    let data = JSON.parse(this.response);
+  let output = '';
+    
+}
+
+  getData.send();
+  console.log(getData)
